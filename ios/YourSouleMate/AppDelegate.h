14995d351bc5ccc862eb973680e5c525fc2fa5f5
hshan <hyeoksu.han@gmail.com>
2014-08-10 18:29:24 +0900

    ios - init

---
+++ ios/YourSouleMate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YourSouleMate
+//
+//  Created by HanHyeoksu on 2014. 8. 10..
+//  Copyright (c) 2014년 pread.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/YourSouleMate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YourSouleMate
+//
+//  Created by HanHyeoksu on 2014. 8. 10..
+//  Copyright (c) 2014년 pread.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
