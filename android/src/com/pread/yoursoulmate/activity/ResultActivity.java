88eec67b97b4b021b1f0842596ef22fd83af7d51
hshan <hyeoksu.han@gmail.com>
2014-08-27 23:18:27 +0900

    android - kakao sdk 포함 및 1차 구현코드에 결과값 kakaostory posting 기능 추가  - git add를 잘못해서 단계적으로 commit하지 못하고 한번에 commit  - 전체적인 refactoring 필요

--- android/src/com/pread/yoursoulmate/activity/ResultActivity.java
+++ android/src/com/pread/yoursoulmate/activity/ResultActivity.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Random;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import com.pread.yoursoulmate.GlobalData;
@@ -18,26 +21,49 @@ public class ResultActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        showResult();
+        setButtons();
+    }
 
-        String result = getResult();
+	private void showResult() {
+		String result = getResult();
         TextView resultTextView = (TextView)findViewById(R.id.tv_result);
         resultTextView.setText(result);
+	}
+	
+	private String getResult() {
+		GlobalData gd = (GlobalData) getApplicationContext();
+		List<String> resultList = gd.getResultList();
+		
+		Random random = new Random(System.currentTimeMillis());
+		int index = random.nextInt(resultList.size());
+		
+		return resultList.get(index);
+	}
+	
+	private void setButtons() {
+		findViewById(R.id.btn_again).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		
+		findViewById(R.id.btn_post_kakaostory).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				TextView resultTextView = (TextView)findViewById(R.id.tv_result);
+		        String content = resultTextView.getText().toString();
+		        Log.e("content", content);
+		        
+		        GlobalData gd = (GlobalData)getApplicationContext();
+		        gd.setResultStr(content);
+		        
+		        Intent intent = new Intent(ResultActivity.this, KakaoStoryLoginActivity.class);
+		        startActivity(intent);
+			}
+		});
+	}
+	
 
-        findViewById(R.id.btn_again).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
-
-    public String getResult() {
-        GlobalData gd = (GlobalData) getApplicationContext();
-        List<String> resultList = gd.getResultList();
-
-        Random random = new Random(System.currentTimeMillis());
-        int index = random.nextInt(resultList.size());
-
-        return resultList.get(index);
-    }
 }
