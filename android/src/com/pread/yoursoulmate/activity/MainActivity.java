c947374328abbd0f82889c1ff0a5b612e0571d21
hshan <hyeoksu.han@gmail.com>
2014-09-19 11:53:43 +0900

    android - 광고를 BANNER에서 SMART_BANNER로 변경, 지문인식 애니메이션 및 이미지 변경  - 이미지 name 및 기타 수정 필요

--- android/src/com/pread/yoursoulmate/activity/MainActivity.java
+++ android/src/com/pread/yoursoulmate/activity/MainActivity.java
@@ -2,45 +2,45 @@ package com.pread.yoursoulmate.activity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.pread.yoursoulmate.R;
-import com.pread.yoursoulmate.view.DrawingView;
 
 public class MainActivity extends Activity {
-	AdView mAdView;
+	private AdView mAdView;
+	
+	private void loadAdView() {
+		AdRequest adRequest = new AdRequest.Builder()
+		.build();
+		
+		mAdView = (AdView)findViewById(R.id.adView);
+		mAdView.loadAd(adRequest);
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		AdRequest adRequest = new AdRequest.Builder()
-        .build();
-
-		mAdView = (AdView)findViewById(R.id.adView);
-		mAdView.loadAd(adRequest);
-		mAdView.bringToFront();
+		loadAdView();
 		
+		final ImageView fingerprint = (ImageView)findViewById(R.id.iv_fingerprint);
+		final Animation alphaAnim = AnimationUtils.loadAnimation(fingerprint.getContext(), R.anim.fingerprint_change_alpha);
 		
-		final DrawingView fingerprint = (DrawingView)findViewById(R.id.dv_fingerprint);
-		fingerprint.setImageResource(R.drawable.q);
-		
-		final View fingerprintShadow = findViewById(R.id.tv_fingerprint_shadow);
+		final View board = findViewById(R.id.iv_fingerprint_scanner_board);
+		final View screen = findViewById(R.id.fingerprint_screen);
 		final View fingerprintScanbar = findViewById(R.id.tv_fingerprint_scanbar); 
 
 		final Animation scanAnimation = AnimationUtils.loadAnimation(this, R.anim.fingerprint_scan);
-
-		final View title = findViewById(R.id.tv_input_fingerprint);
-		title.bringToFront();
 		
 		scanAnimation.setAnimationListener(new AnimationListener() {
 			@Override
@@ -52,13 +52,10 @@ public class MainActivity extends Activity {
 			@Override
 			public void onAnimationEnd(Animation animation) {
 				fingerprintScanbar.setVisibility(View.INVISIBLE);
-
-				Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-				startActivity(intent);
 			}
 		});
 
-		fingerprintShadow.setOnTouchListener(new View.OnTouchListener() {
+		board.setOnTouchListener(new View.OnTouchListener() {
 			@SuppressLint("ClickableViewAccessibility") 
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
@@ -66,29 +63,32 @@ public class MainActivity extends Activity {
 
 				switch (action) {
 				case MotionEvent.ACTION_DOWN:
-					fingerprintShadow.startAnimation(scanAnimation);
+					fingerprint.setVisibility(View.VISIBLE);
+					fingerprint.startAnimation(alphaAnim);
+					
+					screen.startAnimation(scanAnimation);
 
 					fingerprintScanbar.setVisibility(View.VISIBLE);
 					fingerprintScanbar.startAnimation(scanAnimation);
-					/* go through */
-				case MotionEvent.ACTION_MOVE:
-					fingerprint.setVisibility(View.VISIBLE);
-					fingerprint.setPosistion(event.getX(), event.getY() + title.getHeight());
-					fingerprint.invalidate();
 					
 					break;
+					
 				case MotionEvent.ACTION_UP:
-
-					fingerprint.setVisibility(View.INVISIBLE);
-					fingerprintShadow.startAnimation(scanAnimation);
-					fingerprintShadow.clearAnimation();
+					
+					fingerprint.setVisibility(
+							fingerprintScanbar.getVisibility() == View.VISIBLE ?
+									View.INVISIBLE : View.VISIBLE);
+					fingerprint.startAnimation(scanAnimation);
+					fingerprint.clearAnimation();
+					
+					screen.startAnimation(scanAnimation);
+					screen.clearAnimation();
 
 					fingerprintScanbar.setVisibility(View.INVISIBLE);
 					fingerprintScanbar.startAnimation(scanAnimation);
 					fingerprintScanbar.clearAnimation();
 
 					break;
-	
 					
 				default:
 					break;
@@ -96,11 +96,8 @@ public class MainActivity extends Activity {
 				return true;
 			}
 		});
-		
-	
 	}  
 	
-    /** Called when leaving the activity */
     @Override
     public void onPause() {
         if (mAdView != null) {
@@ -109,7 +106,6 @@ public class MainActivity extends Activity {
         super.onPause();
     }
 
-    /** Called when returning to the activity */
     @Override
     public void onResume() {
         super.onResume();
@@ -118,7 +114,6 @@ public class MainActivity extends Activity {
         }
     }
 
-    /** Called before the activity is destroyed */
     @Override
     public void onDestroy() {
         if (mAdView != null) {
@@ -126,5 +121,4 @@ public class MainActivity extends Activity {
         }
         super.onDestroy();
     }
-
 }
