26b48acbd8af4239e92b599dcf649008c3c09d6e
yamigo <yamigo1021@gmail.com>
2014-10-06 18:32:56 +0900

    bin 폴더 삭제, kakao samples 삭제

--- android/vendor/kakao-android-sdk-project-1.0.35/samples/kakaostory-sample/src/com/kakao/sample/kakaostory/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * KakaoStory API를 테스트 하는 샘플 앱
- * <br/> profile, upload, post API를 요청한다.
- */
-package com.kakao.sample.kakaostory;
--- android/vendor/kakao-android-sdk-project-1.0.35/samples/push-sample/src/com/kakao/sample/push/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Push API를 테스트 하는 샘플 앱
- * <br/> 푸시토큰 등록, 삭제, 자신에게 푸시 메시지 보내기를 테스트 한다.
- */
-package com.kakao.sample.push;
